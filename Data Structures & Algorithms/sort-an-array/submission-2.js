8dd38fcd18b63e2c4d26f872d9c9c719878029f4
Mansimran-kaur09 <77012184+Mansimran-kaur09@users.noreply.github.com>
2026-07-26 17:01:22 +0530

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    partition(arr, low, high) {
+        const pivot = arr[high];
+        let i = low - 1;
+
+        for (let j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+        }
+        [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+        return i + 1;
+    }
+
+    quickSort(arr, low = 0, high = arr.length - 1) {
+        if (low < high) {
+            const pivotIndex = this.partition(arr, low, high);
+
+            this.quickSort(arr, low, pivotIndex - 1);
+            this.quickSort(arr, pivotIndex + 1, high);
+        }
+        return arr;
+    }
+
+    sortArray(nums) {
+        return this.quickSort(nums);
+    }
+}
