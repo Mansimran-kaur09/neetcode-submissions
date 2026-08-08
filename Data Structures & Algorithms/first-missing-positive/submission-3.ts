257d91561d65cb288a34afc98f8d1ba55f0a0734
Mansimran-kaur09 <77012184+Mansimran-kaur09@users.noreply.github.com>
2026-08-08 10:01:39 +0530

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums: number[]): number {
+        const set: Set<number> = new Set(nums);
+        let present: number = 1;
+
+        for (const num of nums) {
+            if(num === 0 && !set.has(present)) return present
+
+            if (num > 0 && set.has(present)) {
+                present++;
+            };
+        }
+
+        return present;
+    }
+}
