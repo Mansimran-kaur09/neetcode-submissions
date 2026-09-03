b1d8a1b290b2765379fb56ca06ad7a669fa8fdc7
Mansimran-kaur09 <77012184+Mansimran-kaur09@users.noreply.github.com>
2026-09-03 16:43:36 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums: number[]): number[] {
+        const ans: number[] = [...nums];
+
+        for(let i = 0; i<nums.length; i++) {
+            ans.push(nums[i]);
+
+        }
+
+        return ans;
+    }
+}
